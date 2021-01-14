2665b30e6768a3a29a63ff461ca400a6ec414036
cnov20 <chrisnovelli0@gmail.com>
2021-01-14 10:47:11 -0500

    React.js / graphQL API / Hasura based full-stack to-do list prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useQuery, useMutation, gql } from '@apollo/client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GET_TODOS = gql`
+query getTodos {
+  todos {
+    done
+    id
+    text
+  }
+}
+`
+
+const TOGGLE_TODO = gql`
+mutation toggleTodo($id: uuid!, $done: Boolean!) {
+  update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`
+
+const ADD_TODO = gql`
+mutation addTodo($text: String!) {
+  __typename
+  insert_todos(objects: {text: $text}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`
+
+const DELETE_TODO = gql`
+mutation deleteTodo($id: uuid!) {
+  delete_todos(where: {id: {_eq: $id}}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
 }
 
-export default App;
+`
+
+// List todos
+// Add todos
+// Toggle todos
+// Delete todos
+
+function App() {
+  const [todoText, setTodoText] = React.useState('')
+  const { data, loading, error } = useQuery(GET_TODOS)
+  const [toggleTodo] = useMutation(TOGGLE_TODO)
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setTodoText('')
+  })
+  const [deleteTodo] = useMutation(DELETE_TODO)
+
+  async function handleToggleTodo(todo) {
+    const data = await toggleTodo({ variables: { id: todo.id, done: !todo.done } })
+    console.log('TOGGLED: ', data)
+  }
+
+  async function handleAddTodo(event) {
+    event.preventDefault()
+    if (!todoText.trim()) return
+    const data = await addTodo({
+      variables: { text: todoText },
+      refetchQueries: [{ query: GET_TODOS }]
+    })
+    console.log('ADDED: ', data)
+    // setTodoText('')
+  }
+
+  async function handleDeleteTodo({ id }) {
+    const isConfirmed = window.confirm('Do you want to delete this todo?')
+    if (isConfirmed) {
+      const data = await deleteTodo({
+        variables: { id },
+        update: cache => {
+          const prevData = cache.readQuery({ query: GET_TODOS })
+          const newTodos = prevData.todos.filter(todo => todo.id !== id)
+          cache.writeQuery({ query: GET_TODOS, data: { todos: newTodos } })
+        }
+      })
+      console.log('Deleted Todo: ', data)
+    }
+  }
+
+  if (loading) return <div>Loading todos...</div>
+  if (error) return <div>Error fetching todos!</div>
+
+  return (
+    <div className='vh-100 code flex flex-column items-center bg-purple white pa3 fl-1'>
+      <h1 className='f2-1'>GraphQL Checklist
+        <span role='img' aria-label='Checkmark'>check</span>
+      </h1>
+      {/* Todo Form */}
+      <form onSubmit={handleAddTodo} className='mb3'>
+        <input
+          className='pa2 f4 b--dashed'
+          type='text'
+          placeholder='Write your todo here'
+          onChange={event => setTodoText(event.target.value)}
+          value={todoText}
+        />
+        <button className='pa2 f4 bg-green' type='submit'>Create</button>
+      </form>
+      {/* {Todo List} */}
+      <div className='flex items-center justify-center flex-column'>
+        {data.todos.map(todo => (
+          <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+            <span className={`pointer list pa1 f3 ${todo.done && 'strike'}`}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(todo)} className='bg-transparent bn f4'><span className='red'>&times;</span></button>
+          </p>
+        ))}
+      </div>
+    </div>
+  )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// Setup and instantiate Apollo Client
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+// Add provider for use with React Context hook
+// This is how Apollo manages state throughout our entire application / component tree
+import { ApolloProvider } from '@apollo/client'
+
+// configure endpoint for serverless backend / GraphQL API (Hasura)
+const client = new ApolloClient({
+  uri: 'https://tight-haddock-21.hasura.app/v1/graphql',
+  cache: new InMemoryCache()
+})
+
+// client.query({
+//   query: gql`
+//     query getTodos {
+//   todos {
+//     done
+//     id
+//     text
+//   }
+// }
+//   `
+// }).then(data => console.log(data))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals()
